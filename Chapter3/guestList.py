df53eba4d9c2581cdd408dddeac7894fb1d349bd
Eda AYDIN <edaaydin.ea@outlook.com>
2024-09-26 01:05:50 +0300

    feat: Add code for inviting guests to dinner
    This commit adds code to invite guests to dinner by displaying an invitation message for each guest in the list.

---
+++ Chapter3/guestList.py
@@ -0,0 +1,6 @@
+# List of guests
+guests = ['Albert Einstein', 'Marie Curie', 'Ada Lovelace']
+
+# Invitation message for each guest
+for guest in guests:
+    print(f"Dear {guest}, you are invited to dinner!")
