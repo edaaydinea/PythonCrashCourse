ae4f38ce18daa0d28df71c401d83a0783e98ced0
Eda AYDIN <edaaydin.ea@outlook.com>
2024-09-26 01:05:40 +0300

    Add code for displaying the number of people invited to dinner

---
+++ Chapter3/dinnerGuests.py
@@ -0,0 +1,5 @@
+# Reusing the guest list from previous exercises
+guests = ['Nikola Tesla', 'Isaac Newton', 'Charles Darwin', 'Marie Curie', 'Ada Lovelace', 'Rosalind Franklin']
+
+# Using len() to print the number of people invited to dinner
+print(f"\nI am inviting {len(guests)} people to dinner.")
