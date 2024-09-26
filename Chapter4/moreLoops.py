160b03e0e7113a3d24ff1c9a5267e768348e7b55
Eda AYDIN <edaaydin.ea@outlook.com>
2024-09-27 00:23:59 +0300

    feat: Add code for printing favorite foods
    This commit adds code to print a list of favorite foods for both the user and their friend. The code uses for loops to iterate over the lists of foods and print each item.

---
+++ Chapter4/moreLoops.py
@@ -0,0 +1,12 @@
+# List of foods
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = ['ice cream', 'pasta', 'salad']
+
+# Printing each list of foods with for loops
+print("My favorite foods are:")
+for food in my_foods:
+    print(food)
+
+print("\nMy friend’s favorite foods are:")
+for food in friend_foods:
+    print(food)
