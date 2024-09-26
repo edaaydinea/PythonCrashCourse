6164f8530bef0f7f046b84048ebfa7042e8b3396
Eda AYDIN <edaaydin.ea@outlook.com>
2024-09-27 00:24:03 +0300

    feat: Add code for managing dimensions
    This commit adds code for managing dimensions. It initializes the dimensions as (200, 50) and prints the original dimensions. Then, it modifies the dimensions to (400, 100) and prints the modified dimensions.

---
+++ Chapter4/dimensions.py
@@ -0,0 +1,9 @@
+dimension = (200, 50)
+print("Original dimensions:")
+for dim in dimension:
+    print(dim)
+    
+dimension = (400, 100)
+print("\nModified dimensions:")
+for dim in dimension:
+    print(dim)
