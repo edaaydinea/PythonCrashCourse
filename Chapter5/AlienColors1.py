59444b59cd2dd856620971717e57610dfa6e91df
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-01 19:53:00 +0300

    feat: Add code for checking alien color and earning points
    This commit adds code to check the color of an alien and earn points based on the color. It includes two versions: one that passes the if test with a green alien color, and one that fails the if test with a red alien color.
    
    Ref: #123

---
+++ Chapter5/AlienColors1.py
@@ -0,0 +1,13 @@
+# Version that passes the if test
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+# Version that fails the if test
+alien_color = 'red'
+
+if alien_color == 'green':
+    # No output since the condition is not met
+    print("You just earned 5 points!")
+
