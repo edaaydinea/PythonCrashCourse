75c53c1a76295c44ea2d0801325050b0ca9ccc99
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-01 19:53:10 +0300

    feat: Add code for checking alien color and earning points

---
+++ Chapter5/AlienColors2.py
@@ -0,0 +1,15 @@
+# Version that runs the if block
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien!")
+else:
+    print("You just earned 10 points for shooting the alien!")
+
+# Version that runs the else block
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien!")
+else:
+    print("You just earned 10 points for shooting the alien!")
