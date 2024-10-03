81fb1043ac3dc0b643beb7f246aec99a1aa8226b
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-04 00:24:49 +0300

    Create formatted_name.py

---
+++ Chapter8/formatted_name.py
@@ -0,0 +1,29 @@
+def get_formatted_name(first_name, last_name):
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
+
+def get_formatted_name2(first_name, middle_name, last_name):
+    full_name = f"{first_name} {middle_name} {last_name}"
+    return full_name.title()
+
+
+musician = get_formatted_name2('john', 'lee', 'hooker')
+print(musician)
+
+def get_formatted_name3(first_name, last_name, middle_name=""):
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
+
+    return full_name.title()
+
+musician = get_formatted_name3("jimi", "hendrix")
+print(musician)
+
+musician = get_formatted_name3("john", "hooker", "lee")
