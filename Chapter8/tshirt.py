d45225c2bd2040942f28c761255462358e6291a5
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-04 00:25:00 +0300

    Create tshirt.py

---
+++ Chapter8/tshirt.py
@@ -0,0 +1,5 @@
+def make_shirt(size, message):
+    print(f"\nThis T-shirt is size {size} and has the message '{message}'.")
+
+make_shirt('medium', 'I love Python programming!')
+make_shirt(message='Stay positive!', size='large')
