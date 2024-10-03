3f027169d7b8b8c81790ec5d797647f2c0ca7e4e
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-04 01:00:21 +0300

    Add show_messages function to display a list of text messages

---
+++ Chapter8/messages.py
@@ -0,0 +1,10 @@
+def show_messages(messages):
+    """Print each message in the list."""
+    for message in messages:
+        print(message)
+
+# List of text messages
+text_messages = ["Hello, how are you?", "Don't forget to call me!", "Happy Birthday!"]
+
+# Call the function
+show_messages(text_messages)
