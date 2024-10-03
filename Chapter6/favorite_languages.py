d738514371d147c1b4053bff74b23906dbfe85f5
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-03 19:47:59 +0300

    Add code for displaying Sarah's favorite language

---
+++ Chapter6/favorite_languages.py
@@ -0,0 +1,12 @@
+# A Dictionary of Similar Objects
+print("\nA Dictionary of Similar Objects")
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+languages = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {languages}.")
