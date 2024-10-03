311052de54263964818ec5b0066fa01662bb6395
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-03 20:49:47 +0300

    Add code for rental_car.py to ask user for rental car preference and display a message with their choice

---
+++ Chapter7/rental_car.py
@@ -0,0 +1,5 @@
+# Ask the user what kind of rental car they would like
+car = input("What kind of rental car would you like? ")
+
+# Print a message with their choice
+print(f"Let me see if I can find you a {car}.")
