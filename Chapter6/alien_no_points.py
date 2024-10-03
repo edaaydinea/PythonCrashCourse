1ebab40fd7589f0ecd9197683c9c5b96dd98964c
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-03 19:47:39 +0300

    Add code for accessing values in a dictionary using get()

---
+++ Chapter6/alien_no_points.py
@@ -0,0 +1,6 @@
+# Using get() to access values in a dictionary
+print("Using get() to access values in a dictionary")
+
+alien_0 = {'color': 'green', 'speed': 'slow'}
+point_value = alien_0.get('points', 'No point value assigned.')
+print(point_value)
