eaa30e3e81baa4534c74bd91e2327a163c741823
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-04 01:00:01 +0300

    Add city_names.py with city_country function and example usage

---
+++ Chapter8/city_names.py
@@ -0,0 +1,6 @@
+def city_country(city, country):
+    return city.title() + ', ' + country.title()
+
+print(city_country('santiago', 'chile'))
+print(city_country('ushuaia', 'argentina'))
+print(city_country('buenos aires', 'argentina'))
