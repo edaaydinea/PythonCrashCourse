207b4d2368edaf6d80a4e1bf4b92f6cd514101f3
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-04 00:24:57 +0300

    Create message.py

---
+++ Chapter8/message.py
@@ -0,0 +1,6 @@
+def display_message():
+    """Print a message about what you're learning."""
+    print("In this chapter, I am learning about functions in Python.")
+
+# Call the function
+display_message()
