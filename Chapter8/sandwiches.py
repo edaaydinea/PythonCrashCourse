cfbc557595c022b8ba5faacc5ee7f5e036207ceb
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-04 01:02:35 +0300

    Add functions to create and display sandwich models in sandwiches.py

---
+++ Chapter8/sandwiches.py
@@ -0,0 +1,8 @@
+def make_sandwich(*items):
+    print("\nMaking a sandwich with the following items:")
+    for item in items:
+        print("- " + item)
+
+make_sandwich('roast beef', 'cheddar cheese', 'lettuce', 'honey dijon')
+make_sandwich('turkey', 'apple slices', 'honey mustard')
+make_sandwich('peanut butter', 'strawberry jam')
