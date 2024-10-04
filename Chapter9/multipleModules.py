78952ba4128cce07ca5fe6e1229bc6272b1f93f3
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-05 01:12:16 +0300

    Add multipleModules.py with Admin class and Privileges class implementation

---
+++ Chapter9/multipleModules.py
@@ -0,0 +1,22 @@
+# admin.py
+
+from users import User
+
+class Privileges:
+    """Represent a set of privileges for an admin user."""
+    
+    def __init__(self):
+        """Initialize privileges."""
+        self.privileges = ["can add post", "can delete post", "can ban user"]
+        
+    def show_privileges(self):
+        """Print the list of privileges."""
+        print(f"Administrator Privileges: {', '.join(self.privileges)}")
+
+class Admin(User):
+    """Represent aspects of a user specific to an administrator."""
+    
+    def __init__(self, first_name, last_name):
+        """Initialize attributes of the parent class and admin."""
+        super().__init__(first_name, last_name)
+        self.privileges = Privileges()  # Create an instance of Privileges
