c1895cf8c364a1769c5619c56aa5d5a74edda8a5
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-05 01:11:58 +0300

    Add importedAdmin.py with Admin class usage

---
+++ Chapter9/importedAdmin.py
@@ -0,0 +1,9 @@
+# admin_test.py
+
+from user2 import Admin
+
+# Create an instance of Admin
+admin_user = Admin("Alice", "Smith")
+
+# Call the method to show privileges
+admin_user.privileges.show_privileges()
