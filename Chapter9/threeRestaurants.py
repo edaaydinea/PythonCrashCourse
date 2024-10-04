25c07e847736311f20d2fe568d9ae3f66fad2cac
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-05 01:13:38 +0300

    Add code to create and describe three different instances of the Restaurant class

---
+++ Chapter9/threeRestaurants.py
@@ -0,0 +1,11 @@
+from restaurant import Restaurant
+
+# Creating three different instances of the Restaurant class
+restaurant_1 = Restaurant('Sushi Zen', 'Japanese')
+restaurant_2 = Restaurant('Pasta House', 'Italian')
+restaurant_3 = Restaurant('Spice Villa', 'Indian')
+
+# Call describe_restaurant for each instance
+restaurant_1.describe_restaurant()
+restaurant_2.describe_restaurant()
+restaurant_3.describe_restaurant()
