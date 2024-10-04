45c1d506fb6039050b806c9a89094ace8629171f
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-05 01:12:02 +0300

    Add importedRestaurant.py with Restaurant class usage

---
+++ Chapter9/importedRestaurant.py
@@ -0,0 +1,9 @@
+# main.py
+
+from restaurant import Restaurant
+
+# Create an instance of Restaurant
+my_restaurant = Restaurant("The Gourmet Place", "Italian")
+
+# Call a method from the Restaurant class
+my_restaurant.describe_restaurant()
