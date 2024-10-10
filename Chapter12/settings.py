755ba0a07956a2a547ce8e593dc3ae52f1403ca7
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-10 20:14:54 +0300

    feat: Add game settings class and its properties
    This commit adds a new class called Settings that is responsible for storing all the settings for the Alien Invasion game. It includes properties for screen dimensions, background color, image paths for the ship and character, ship speed, bullet speed, bullet dimensions, bullet color, and the maximum number of allowed bullets.
    
    The purpose of this commit is to provide a centralized location for managing the game settings, making it easier to modify and customize the game behavior.

---
+++ Chapter12/settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+    
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230) # a light gray background color.
+        #self.bg_color2 = (135, 206, 250) # a sky blue background color.
+        
+        # Images
+        self.ship_image = './Chapter12/images/ship.bmp'
+        self.character_image = './Chapter12/images/monster.bmp'
+        
+        # Ship settings
+        self.ship_speed = 1.5
+        
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+        
