2f6efeae1ab0577d151710934929d2ef732bfc3c
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-16 15:26:05 +0300

    feat: Add script to plot daily high and low temperatures for Sitka in 2018

---
+++ Chapter16/sitka_highs_lows.py
@@ -0,0 +1,37 @@
+import csv
+import matplotlib.pyplot as plt
+from save_fig import SaveFig
+from datetime import datetime
+
+#filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # Get dates, and high and low temperatures from this file.
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[5])
+        low = int(row[6])
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+        
+# Plot the high and lows temperatures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# Format plot.
+plt.title("Daily high and low temperatures, 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+save_fig = SaveFig()
+save_fig.save_fig('sitka_highs_lows1.png')
