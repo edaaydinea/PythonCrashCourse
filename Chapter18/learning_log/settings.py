2afd431b766226906ff0cfca1989e5f4474290cf
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-19 00:47:44 +0300

    Refactor secret key handling in settings.py

--- Chapter18/learning_log/settings.py
+++ Chapter18/learning_log/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v-_+u1vh9hlgu=!lm18d^h22mtt9y_51rikpxmtcc5u(o47t$f'
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
